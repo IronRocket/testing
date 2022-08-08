52f20780589973fba9f16b2d90145b9f637d3ec5
Iron Rocket <buzzbzmarch02@gmail.com>
2022-08-07 22:03:42 -0700

    revision

--- math.sh
+++ math.sh
@@ -9,19 +9,23 @@ do
     clear
     echo "Are you ready?"
     read
+    #Generating Random Numbers
     rand=$(($startn + $RANDOM % $endn))
     rand_2=$(($startn + $RANDOM % $endn))
     echo "What is ${rand}+${rand_2}"
 
+    
     start=$(date +'%s')
     read z
     end=$(date +'%s')
     total=$((rand+rand_2))
+
     if [ $z == $total ]; then
         echo "COrect"
     else
         echo "Nope"
     fi
+    #Calculating duration it took to give input on line 19
     duration=$(($end-$start))
     echo "It took you ${duration} seconds to answer the question"
     read
