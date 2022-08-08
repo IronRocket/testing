a6245edb4bee3095ab890ef2c628747d64b75a77
Iron Rocket <buzzbzmarch02@gmail.com>
2022-08-07 21:47:08 -0700

    learning bash

---
+++ yo.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+echo "Time to set your range"
+echo "Starting number"
+read startn
+echo "Ending numeber"
+read endn
+while [ true ] 
+do
+    clear
+    echo "Are you ready?"
+    read
+    rand=$(($startn + $RANDOM % $endn))
+    rand_2=$(($startn + $RANDOM % $endn))
+    echo "What is ${rand}+${rand_2}"
+
+    start=$(date +'%s')
+    read z
+    end=$(date +'%s')
+    total=$((rand+rand_2))
+    if [ $z == $total ]; then
+        echo "COrect"
+    else
+        echo "Nope"
+    fi
+    duration=$(($end-$start))
+    echo "It took you ${duration} seconds to answer the question"
+    read
+done
